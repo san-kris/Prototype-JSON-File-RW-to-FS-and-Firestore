728314063834441ca5beeda1e273801843cb598c
Santosh Krishnamurthy <san.b350@gmail.com>
2023-01-30 13:18:45 -0600

    Initial Commit

---
+++ Proto File RW plus Firestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proto File RW plus Firestore
+//
+//  Created by Santosh Krishnamurthy on 1/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
